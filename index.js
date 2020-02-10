9513dd378e8355df34cabd9345ca9aaca12a000a
Christopher Floyd <cfloyd252@gmail.com>
2020-02-10 14:22:06 -0700

    completed Jedi name

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+/* eslint-disable no-console */
 'use strict';
+
+function jediName(firstName, lastName) {
+  return (`${lastName.slice(0,3)}${firstName.slice(0,2)}`);
+}
+
+const myJediName = jediName('Beyonce','Knowles');
+
+console.log(myJediName);
