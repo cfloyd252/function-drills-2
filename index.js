9e0b4aafadcdd772632c3fa333847d341e7606e1
Christopher Floyd <cfloyd252@gmail.com>
2020-02-10 15:18:18 -0700

    completed cracking the code

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 /* eslint-disable no-console */
 'use strict';
 
-function beyond (num) {
-  if (num === Infinity || num === -Infinity) {
-    console.log('And beyond');
-  } else if (num > 0) {
-    console.log('To infinity');
-  } else if (num < 0) {
-    console.log('To negative infinity');
-  } else if (num === 0) {
-    console.log('Stay home');
+function decode (encodedWord) {
+  const firstLetter = encodedWord.slice(0,1);
+  if (firstLetter === 'a') {
+    return encodedWord.slice(1, 2);
+  } else if (firstLetter === 'b') {
+    return encodedWord.slice(2,3);
+  } else if (firstLetter === 'c') {
+    return encodedWord.slice(3,4);
+  } else if (firstLetter === 'd') {
+    return encodedWord.slice(4,5);
+  } else {
+    return ' ';
   }
 }
 
-beyond(0);
+const craft = decode('craft');
+const block = decode('block');
+const argon = decode('argon');
+const meter = decode('meter');
+const bells = decode('bells');
+const brown = decode('brown');
+const croon = decode('croon');
+const droop = decode('droop');
+
+console.log(craft,block,argon,meter,bells,brown,croon,droop);
